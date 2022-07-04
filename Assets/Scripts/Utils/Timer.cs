67d3d03572fd31e1a49b20801284bae0b75df22e
Victor Gabler <vfgabler@gmail.com>
2022-07-04 11:41:48 -0300

    Setup testes unitarios e testes do component Timer

---
+++ Assets/Scripts/Utils/Timer.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace InfinityZombies.Utils
+{
+    public class Timer : MonoBehaviour
+    {
+        float timer;
+        float currentDuration;
+
+        [field: SerializeField]
+        public float DefaultDuration { get; set; } = 1;
+        [field: SerializeField]
+        public bool AutoPlay { get; set; } = false;
+        [field: SerializeField]
+        public bool UseUnscaledTime { get; set; } = false;
+
+        public UnityEvent OnBegin = new UnityEvent();
+        public UnityEvent OnEnd = new UnityEvent();
+
+        public bool IsRunning { get; set; }
+        public float RemainingTime => timer;
+
+        private void Start()
+        {
+            if (AutoPlay)
+            {
+                Activate();
+            }
+        }
+
+        public void Restart()
+        {
+            Restart(currentDuration);
+        }
+
+        public void Restart(float duration)
+        {
+            Stop();
+            Activate(duration);
+        }
+
+        public void Activate(float duration)
+        {
+            if (IsRunning)
+            {
+                return;
+            }
+
+            currentDuration = duration;
+            timer = duration;
+            IsRunning = true;
+            OnBegin?.Invoke();
+        }
+
+        public void Activate()
+        {
+            Activate(DefaultDuration);
+        }
+
+        public void Stop()
+        {
+            IsRunning = false;
+            timer = 0;
+        }
+
+        public void End()
+        {
+            Stop();
+            OnEnd?.Invoke();
+        }
+
+        private void Update()
+        {
+            if (!IsRunning)
+            {
+                return;
+            }
+
+            timer -= UseUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
+            if (timer <= 0)
+            {
+                End();
+            }
+        }
+    }
+}
---
+++ Assets/Tests/Play Mode/Utils/TimerTests.cs
@@ -0,0 +1,173 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using InfinityZombies.Utils;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TimerTests
+{
+    enum TimerState { Idle, Started, Ended, Running };
+
+    Timer timer;
+    bool started;
+    bool ended;
+
+    TimerState state
+    {
+        get
+        {
+            if (!timer.IsRunning)
+            {
+                if (ended)
+                {
+                    return TimerState.Ended;
+                }
+
+                return TimerState.Idle;
+            }
+
+            if (started)
+            {
+                return TimerState.Started;
+            }
+
+            return TimerState.Running;
+        }
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        timer = new GameObject().AddComponent<Timer>();
+
+        timer.OnBegin.AddListener(() => started = true);
+        timer.OnEnd.AddListener(() => ended = true);
+        started = ended = false;
+    }
+
+    [UnityTest]
+    public IEnumerator Should_run_until_the_end()
+    {
+        Assert.That(state, Is.EqualTo(TimerState.Idle));
+
+        //Ao ativar verifica que iniciou
+        timer.Activate();
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+
+        //depois do tempo padrão, verifica que terminou
+        yield return new WaitForSeconds(timer.DefaultDuration);
+
+        Assert.That(state, Is.EqualTo(TimerState.Ended));
+    }
+
+    [UnityTest]
+    public IEnumerator Should_start_automatically_with_default_duration()
+    {
+        //Precisa chamar o setup aqui, porque roda um frame antes de iniciar o teste / depois do setup
+        //Sendo assim não adiantaria definir o AutoPlay porque o start já foi
+        Setup();
+        timer.AutoPlay = true;
+
+        //Verifica que não foi iniciado
+        Assert.That(state, Is.EqualTo(TimerState.Idle));
+        Assert.That(timer.RemainingTime, Is.LessThanOrEqualTo(0));
+
+        //Verifica que depois do primeiro frame iniciou automaticamente
+        yield return new WaitForEndOfFrame();
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+        Assert.That(timer.RemainingTime, Is.GreaterThanOrEqualTo(timer.DefaultDuration));
+    }
+
+    [Test]
+    public void Should_activate_with_custom_duration()
+    {
+        var customDuration = 2;
+        timer.Activate(customDuration);
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+        Assert.That(timer.RemainingTime, Is.GreaterThanOrEqualTo(customDuration));
+    }
+
+    [UnityTest]
+    public IEnumerator Should_restart_with_current_duration()
+    {
+        var customDuration = 2f;
+
+        timer.Activate(customDuration);
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+
+        //Espera metade do tempo, verifica que não terminou
+        yield return new WaitForSeconds(customDuration / 2);
+        Assert.That(state, !Is.EqualTo(TimerState.Ended));
+
+        //Verifica que o start foi chamado denovo, e o end não
+        started = false;
+        ended = false;
+        timer.Restart();
+
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+        Assert.That(ended, Is.EqualTo(false));
+        Assert.That(timer.RemainingTime, Is.GreaterThanOrEqualTo(customDuration));
+    }
+
+    [Test]
+    public void Should_restart_with_custom_duration()
+    {
+        var customDuration = 2f;
+
+        timer.Activate(timer.DefaultDuration);
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+        Assert.That(timer.RemainingTime, Is.GreaterThanOrEqualTo(timer.DefaultDuration));
+
+        //Verifica que o start foi chamado denovo, e o end não
+        started = false;
+        ended = false;
+        timer.Restart(customDuration);
+
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+        Assert.That(ended, Is.EqualTo(false));
+        Assert.That(timer.RemainingTime, Is.GreaterThanOrEqualTo(customDuration));
+    }
+
+    [UnityTest]
+    public IEnumerator Should_update_remaining_time()
+    {
+        Assert.That(timer.RemainingTime, Is.LessThanOrEqualTo(0));
+        timer.Activate();
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+        Assert.That(timer.RemainingTime, Is.GreaterThanOrEqualTo(timer.DefaultDuration));
+
+        yield return new WaitForSeconds(timer.DefaultDuration / 2);
+
+        Assert.That(timer.RemainingTime, Is.LessThanOrEqualTo(timer.DefaultDuration / 2));
+    }
+
+    [Test]
+    public void Should_stop()
+    {
+        timer.Activate();
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+        Assert.That(timer.RemainingTime, Is.GreaterThanOrEqualTo(timer.DefaultDuration));
+
+        //Ao chamar o Stop, não deve ter callback
+        timer.Stop();
+        Assert.That(timer.IsRunning, Is.EqualTo(false));
+        Assert.That(ended, Is.EqualTo(false));
+        Assert.That(timer.RemainingTime, Is.LessThanOrEqualTo(0));
+    }
+
+    [Test]
+    public void Should_end()
+    {
+        timer.Activate();
+        Assert.That(state, Is.EqualTo(TimerState.Started));
+        Assert.That(timer.RemainingTime, Is.GreaterThanOrEqualTo(timer.DefaultDuration));
+
+        //Ao chamar o End, precisa ter o callback
+        timer.End();
+        Assert.That(state, Is.EqualTo(TimerState.Ended));
+        Assert.That(timer.RemainingTime, Is.LessThanOrEqualTo(0));
+    }
+
+}
